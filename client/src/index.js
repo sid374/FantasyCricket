fde4e81b6bedfed5350155a1f797510d5273b8ea
sid374 <sid.bidasaria@gmail.com>
2016-12-22 11:30:54 -0500

    material-ui dep

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import AppRoot from './Components/AppRoot';
 
 import './stylesheets/index.css';
-import 'font-awesome/css/font-awesome.css'
+
 
 ReactDOM.render(
   <AppRoot />,
